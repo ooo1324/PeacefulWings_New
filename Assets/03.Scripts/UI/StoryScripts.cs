f199c01714fa779440e64642b6dc9d150597f54f
ooo1324 <ooo1324@daum.net>
2023-08-04 01:03:00 +0900

    refactor : StoryScripts 개선
    - story 인스펙터에서 좀 더 사용하기 쉽도록 개선

--- Assets/03.Scripts/UI/StoryScripts.cs
+++ Assets/03.Scripts/UI/StoryScripts.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StoryScripts : MonoBehaviour
 {
-    public List<string> talkData;
-    public List<Sprite> spriteList;
+    public List<StoryData> story;
     public Text text;
     public Image storyImg;
 
@@ -36,24 +36,42 @@ public class StoryScripts : MonoBehaviour
         }
     } 
 
+    /// <summary>
+    /// 스토리 재생 함수
+    /// </summary>
+    /// <param name="idx">재생 스토리 index</param>
     public void StoryStart(int idx)
     {
-        if (talkData.Count > idx)
+        if (story.Count > idx)
         {
-            if (spriteList[idx] == null)
+            if (story[idx].storySprite == null)
                 storyImg.color = Color.black;
             else
                 storyImg.color = Color.white;
 
-            storyImg.sprite = spriteList[idx];
-            textEffect.SetMsg(talkData[idx]);
+            storyImg.sprite = story[idx].storySprite;
+            textEffect.SetMsg(story[idx].storyText);
         }
         else
         {
             //Next Scene
-            SceneManager.LoadScene("Stage1Scene");
+            //SceneManager.LoadScene("Stage1Scene");
+            SceneManager.LoadScene("TestScene");
             AllGameData.Current_Level = 1;
         }
     }
 }
- 
+
+/// <summary>
+/// 스토리 구조체
+/// </summary>
+[System.Serializable]
+public struct StoryData
+{
+    //해당 스토리의 이미지
+    public Sprite storySprite;
+
+    //해당 스토리 텍스트
+    [TextArea(3, 5)]
+    public string storyText;
+}
