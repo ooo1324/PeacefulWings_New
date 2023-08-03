352799313bd7719183aebfd00009fa729b01b9eb
ooo1324 <ooo1324@daum.net>
2023-08-03 23:06:49 +0900

    Feat : 스토리 도시 기능 추가
    - 스토리 씬 스크립트 추가
    - 텍스트 효과 스크립트 추가
    - Level1 씬 연결

--- Assets/03.Scripts/UI/StoryScripts.cs
+++ Assets/03.Scripts/UI/StoryScripts.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StoryScripts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<string> talkData;
+    public List<Sprite> spriteList;
+    public Text text;
+    public Image storyImg;
+
+    TextEffect textEffect;
+    private int idx = 0;
+
+    private void Awake()
     {
-        
+        textEffect = text.GetComponent<TextEffect>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StoryStart(idx);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+            if (!textEffect.isAnimation)
+            {
+                idx++;
+            }
+            StoryStart(idx); 
+        }
+    } 
+
+    public void StoryStart(int idx)
+    {
+        if (talkData.Count > idx)
+        {
+            if (spriteList[idx] == null)
+                storyImg.color = Color.black;
+            else
+                storyImg.color = Color.white;
+
+            storyImg.sprite = spriteList[idx];
+            textEffect.SetMsg(talkData[idx]);
+        }
+        else
+        {
+            //Next Scene
+            SceneManager.LoadScene("Stage1Scene");
+            AllGameData.Current_Level = 1;
+        }
     }
 }
+ 
---
+++ Assets/03.Scripts/UI/TextEffect.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextEffect : MonoBehaviour
+{
+    public int CharPerSec;
+    public GameObject EndObj;
+
+    Text msgText;
+    int idx;
+    float interval;
+    string targetMsg;
+
+    [HideInInspector]
+    public bool isAnimation;
+
+    private bool isStop = false;
+    private void Awake()
+    {
+        msgText = GetComponent<Text>();
+    }
+
+    public void SetMsg(string msg)
+    {
+        if (!isAnimation)
+        {
+            targetMsg = msg;
+            EffectStart();
+        }
+        else
+        {
+            Debug.Log("Stop Corutine");
+            StopAllCoroutines();
+            msgText.text = targetMsg;
+            EffectEnd();
+        }
+    }
+
+    void EffectStart()
+    {
+        Debug.Log("text Start");
+        isAnimation = true;
+        msgText.text = "";
+
+        EndObj.SetActive(false);
+
+        interval = 1.0f / CharPerSec;
+
+        StartCoroutine(Effecting());
+    }
+
+    IEnumerator Effecting()
+    {
+        for (int i = 0; i < targetMsg.Length; i++)
+        {
+            msgText.text += targetMsg[i];
+            //Sound
+            //if (targetMsg[index] != ' ' && targetMsg[index] != '.')
+            //    audioSource.Play();
+
+            yield return new WaitForSeconds(interval);
+        }
+
+        EffectEnd();
+    }
+
+    void EffectEnd()
+    {
+        isAnimation = false;
+        EndObj.SetActive(true);
+    }
+}
